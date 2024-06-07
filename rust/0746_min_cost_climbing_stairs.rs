5690bb9ceadc367d65dd68359d3f81645b2324d9
CarrotDLaw <dannylaw0514@gmail.com>
2024-06-08 00:19:05 +0800

    feat: ✨ update files

--- rust/0746_min_cost_climbing_stairs.rs
+++ rust/0746_min_cost_climbing_stairs.rs
@@ -2,7 +2,7 @@ impl Solution {
   pub fn min_cost_climbing_stairs(mut cost: Vec<i32>) -> i32 {
     let n = cost.len();
 
-    for i in (0..n-2).rev() {
+    for i in (0..n - 2).rev() {
       cost[i] += i32::min(cost[i + 1], cost[i + 2]);
     }
 
